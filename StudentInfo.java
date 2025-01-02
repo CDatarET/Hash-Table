ddc08f3247c0a73ff6aad54106b74f9c4c82009e
CDatarET <datar.chaitanya@gmail.com>
2025-01-02 23:06:15 +0530

    Add files via upload

---
+++ StudentInfo.java
@@ -0,0 +1,12 @@
+public class StudentInfo {
+	public int studentNum;
+	public String firstName;
+	public String lastName;
+	
+	public StudentInfo(int studentNum, String firstName, String lastName) {
+		this.studentNum = studentNum;
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+    
+}
